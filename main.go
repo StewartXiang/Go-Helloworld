6343e4d8d3b31b8437a0219c9a4eac9cb4930ed6
xiangnan xu <1351863264@qq.com>
2021-02-04 11:07:00 +0800

    ok with xml

--- main.go
+++ main.go
@@ -6,53 +6,41 @@ import (
 )
 
 func main()  {
-	a := JsonMessage{
-		CDATA{"wer<werw>"},
-		1000,
-		"ffd",
-	}
-	a2 := JsonMessage2{
-		"wer<werwer>",
+	a := XMLMessage{
+		"sdf<sdf>",
 		1000,
 		"ffd",
 	}
 	aByte, _ := xml.Marshal(a)
 	aStr := string(aByte)
 	fmt.Printf("a1: %s\n", aStr)
-	a2Byte, _ := xml.Marshal(a2)
-	a2Str := string(a2Byte)
-	fmt.Printf("a2: %s\n", a2Str)
 
 	bStr := `
-<JsonMessage><my_name><![CDATA[wer<werw>]]></my_name><address>1000</address><phone>ffd</phone></JsonMessage>
+<XMLMessage><my_name><![CDATA[wer<werw>]]></my_name><address>1000</address><phone>ffd</phone></XMLMessage>
 `
-	var b JsonMessage
+	var b XMLMessage
 	err := xml.Unmarshal([]byte(bStr), &b)
-	fmt.Println(bStr)
-	//<JsonMessage>
-	//		<my_name>
-	//			<![CDATA[wer<werw>]]>
-	//		</my_name>
-	//		<address>1000</address><phone>ffd</phone>
-	//</JsonMessage>
 	fmt.Println(b.Name)
-	// 返回{wer<werw>}
-	fmt.Println(b.Name.Text)
-	// 返回wer<werw>
+	// wer<werwer>
 	fmt.Println(err)
 }
 
-type JsonMessage struct {
+//这才可以
+type XMLMessage struct {
 	Name CDATA `xml:"my_name"`
 	Address int `xml:"address"`
 	Phone string `xml:"phone"`
 }
-
-type CDATA struct {
-	Text string `xml:",cdata"`
+type CDATA string
+func (n CDATA) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return e.EncodeElement(struct{
+		S string `xml:",innerxml"`
+	}{
+		S: "<![CDATA[" + string(n) + "]]>",
+	}, start)
 }
 
-
+// 这种不行
 type JsonMessage2 struct {
 	Name string `xml:"my_name>,cdata"`
 	Address int `xml:"address"`
